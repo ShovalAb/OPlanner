e5870be3894ce743bc610b9a57ae5142fcf0a0ec
Yuval Ronen <yuval@interionet.com>
2023-08-05 11:21:58 +0300

    Added handling POST CORS

--- fake_http_server.py
+++ fake_http_server.py
@@ -8,9 +8,19 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         self.send_response(200, "ok")
         self.end_headers()
 
+    def do_POST(self):
+        self.send_response(200, "ok")
+        self.send_header('Content-Type', 'application/json')
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        print("Got from client - " + post_body.decode())
+        self.end_headers()
+        self.wfile.write(b'{"ok":1,"courses-must":[]}')
+
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods',"GET, PUT, POST, DELETE, OPTIONS")
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
