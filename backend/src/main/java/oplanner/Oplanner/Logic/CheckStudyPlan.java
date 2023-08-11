d6cc0a9821cd3e13581062c8d9f9dd4104fb3d84
MayaGur <mayagur98@gmail.com>
2023-08-11 11:01:28 +0300

    small changes

--- backend/src/main/java/oplanner/Oplanner/Logic/CheckStudyPlan.java
+++ backend/src/main/java/oplanner/Oplanner/Logic/CheckStudyPlan.java
@@ -4,21 +4,25 @@ import java.util.Arrays;
 
 import oplanner.Oplanner.Model.Course;
 import oplanner.Oplanner.Model.CreditsRequirement;
+import oplanner.Oplanner.Model.Dependency;
 import oplanner.Oplanner.Model.MandatoryRequirement;
 import oplanner.Oplanner.Model.StudyPlan;
 import oplanner.Oplanner.repository.CourseRepository;
 import oplanner.Oplanner.repository.CreditsRequirementRepository;
 import oplanner.Oplanner.repository.MandatoryRequirementRepository;
 import oplanner.Oplanner.repository.StudyPlanRepository;
+import oplanner.Oplanner.repository.DependencyRepository;
 
 public class CheckStudyPlan {
 
     private final MandatoryRequirementRepository mr;
+    private final DependencyRepository dep;
     private final CourseRepository course;
 
-    public CheckStudyPlan(MandatoryRequirementRepository mr, CourseRepository course)
+    public CheckStudyPlan(MandatoryRequirementRepository mr, DependencyRepository dep, CourseRepository course)
     {
         this.mr = mr;
+        this.dep = dep;
         this.course = course;
     }
 
@@ -57,5 +61,14 @@ public class CheckStudyPlan {
 
         return missingCourses;
     }
+
+    // public Dependency[] checkDependencies (int studyPlanId, Course[] courses)
+    // {
+    //     Dependency[] missingDependencies = new Dependency[0];
+    //     for (Course course : courses)
+    //     {
+    //         Dependency[] depForThisCourse = dep.findByCourseId(course.getId());
+    //     }
+    // }
 }
 
