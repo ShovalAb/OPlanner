83b7ed4b54aed0886b7202bb4a90d5ab790e3c8c
Yuval Ronen <yuval@interionet.com>
2023-08-12 13:03:28 +0300

    Updated fake python server for the new var names

--- fake_http_server.py
+++ fake_http_server.py
@@ -16,14 +16,14 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         print("Got from client - " + post_body.decode())
         self.end_headers()
         if "20406" in post_body.decode():
-            self.wfile.write(b'{"ok":1,"courses-must":[],"courses-depen":[],"nakaz-req":[]}')
+            self.wfile.write(b'{"ok":1,"coursesMust":[],"coursesDepen":[],"creditsReq":[]}')
         else:
-            self.wfile.write(b'{"ok":0,"courses-must":[20406,20407],"courses-depen":[[20406,20416]],"nakaz-req":[{"groupName":"Math","Razui":80,"Mazui":74}]}')
+            self.wfile.write(b'{"ok":0,"coursesMust":[20406,20407],"coursesDepen":[[20406,20416]],"creditsReq":[{"creditType":"Math","neededCredits":80,"currentCredits":74}]}')
 
 
     def do_GET(self):
         print(self.path)
-        if self.path == '/api/studyplan?planId=1':
+        if self.path == '/api/course?planId=1':
             self.send_response(200, 'ok')
             self.end_headers()
             self.wfile.write(open('./api/courseGroup/1', 'rb').read())
