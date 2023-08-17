0ebdf805cff034de561ae92f7a650fec16227223
shovalabudaram <shoval.abudarham@perception-point.io>
2023-08-17 20:43:41 +0300

    change the params of credit req

--- backend/src/main/java/oplanner/Oplanner/Controller/CreditsController.java
+++ backend/src/main/java/oplanner/Oplanner/Controller/CreditsController.java
@@ -1,12 +1,22 @@
 package oplanner.Oplanner.Controller;
 
+import oplanner.Oplanner.Model.Course;
 import oplanner.Oplanner.Model.CreditsRequirement;
+import oplanner.Oplanner.Response.CheckStudyPlanRespone;
+import oplanner.Oplanner.repository.CourseRepository;
 import oplanner.Oplanner.repository.CreditsRequirementRepository;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -15,9 +25,11 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api/credits")
 public class CreditsController {
     private final CreditsRequirementRepository creditsRepository;
+    private final CourseRepository courseRepository;
 
-    public CreditsController(CreditsRequirementRepository creditsRepository) {
+    public CreditsController(CreditsRequirementRepository creditsRepository, CourseRepository courseRepository) {
         this.creditsRepository = creditsRepository;
+        this.courseRepository = courseRepository;
     }
 
     /**
@@ -44,21 +56,17 @@ public class CreditsController {
     /**
      * Get credits requirements by course number.
      *
-     * @param courseNumber Course number.
-     * @return Array of credits requirements for the given course number.
+     * @param selectedCourses Map containing planId and courses list.
+     * @return 
      */
-    @RequestMapping(method = RequestMethod.GET, params = "courseNumber")
-    public ResponseEntity<CreditsRequirement[]> getByCourseId(@RequestParam("courseNumber") int courseNumber) {
-        try {
-            CreditsRequirement[] creditsRequirements = creditsRepository.findByPlanId(courseNumber);
-            if (creditsRequirements != null && creditsRequirements.length > 0) {
-                return ResponseEntity.ok(creditsRequirements);
-            } else {
-                return ResponseEntity.notFound().build();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    @PostMapping
+    public CheckStudyPlanRespone getCreditsReq (@RequestBody Map<String, Object> selectedCourses) {
+        int id = Integer.parseInt(selectedCourses.get("planId").toString());
+        List<Course> courses = new ArrayList<>();
+        List<Integer> coursesNumber = (List<Integer>) selectedCourses.get("courses");
+        for (int number : coursesNumber){
+                courses.add(courseRepository.findByNumber(number));
         }
+        return new CheckStudyPlanRespone(id=1, coursesNumber=null, null, null);
     }
 }
