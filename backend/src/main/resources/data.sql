8c37913fcfaaacc22a91ec9c76a156a64a69ea06
Yuval Ronen <yuval@interionet.com>
2023-08-18 15:18:14 +0300

    Added Courses to data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,33 +4,182 @@
 INSERT INTO study_plan (plan_name) VALUES ("מדעי המחשב");
 
 -- Insert data into Course table
-Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מתמטיקה בדידה", 20476, "מתמטיקה", 4);
-Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אלגברה ליניארית 1", 20109, "מתמטיקה", 7);
-Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אלגברה ליניארית 2", 20229, "מתמטיקה", 5);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מתמטיקה בדידה: תורת הקבוצות, קומבינטוריקה ותורת הגרפים",  20476, "מתמטיקה", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אלגברה לינארית 1",  20109, "מתמטיקה", 7);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אלגברה לינארית 2",  20229, "מתמטיקה", 5);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("חשבון אינפיניטסימלי 1",  20474, "מתמטיקה", 7);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("חשבון אינפיניטסימלי 2",  20475, "מתמטיקה", 7);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("הסתברות ומבוא לסטטיסטיקה למדעי המחשב",  20425, "מתמטיקה", 5);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא למדעי המחשב ושפת Java",  20441, "מדעי המחשב", 6);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבני נתונים ומבוא לאלגוריתמים",  20407, "מדעי המחשב", 6);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אלגוריתמים",  20417, "מדעי המחשב", 5);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מעבדה בתכנות מערכות",  20465, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אוטומטים ושפות פורמליות",  20440, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לתורת החישוביות והסיבוכיות",  20585, "מדעי המחשב מתקדם", 5);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("ארגון המחשב",  20471, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("לוגיקה למדעי המחשב",  20466, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מערכות הפעלה",  20594, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("שפות תכנות",  20905, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מערכות בסיסי-נתונים",  20277, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("עקרונות פיתוח מערכות מידע",  20436, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("תורת הקודים",  20296, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אנליזה נומרית 1",  20462, "מדעי המחשב", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לבינה מלאכותית",  20551, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("תכנות מדעי המחשב מתקדם בשפת Java",  20554, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לרשתות תקשורת מחשבים",  20582, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("תכנות מערכות דפנסיבי",  20937, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("גרפיקה ממוחשבת",  20562, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לקריפטוגרפיה",  20580, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("קומפילציה",  20364, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("תכנות מונחה עצמים",  20906, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מערכות נתונים - טכנולוגיות ואלגוריתמים",  20574, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("חישוביות ביולוגית",  20581, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("כריית מידע",  20595, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אנליזה נומרית 2",  20900, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא ללמידה חישובית",  20942, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("רובוטיקה אלגוריתמית",  20944, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לאבטחת המרחב המקוון",  20940, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("מבוא לבדיקות תוכנה (אנגלית) ",  20946, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("אשנב למחקר במדעי המחשב למצטיינים",  20945, "מדעי המחשב מתקדם", 4);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("פרקי העשרה למצטיינים 1",  20996, "מדעי המחשב מתקדם", 1);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("פרקי העשרה למצטיינים 2",  20997, "מדעי המחשב מתקדם", 1);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("פרקי העשרה למצטיינים 3",  20998, "מדעי המחשב מתקדם", 1);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("פרקי העשרה למצטיינים 4",  20999, "מדעי המחשב מתקדם", 1);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בהנדסת תוכנה",  20368, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר באנליזה נומרית",  20369, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בחישוביות",  20370, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר במערכות בסיסי-נתונים",  20371, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בבינה מלאכותית",  20372, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בקריפטוגרפיה",  20374, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בנושא מיוחד במדעי המחשב",  20375, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בתקשורת/אלגוריתמים מבוזרים",  20388, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר במערכות הפעלה",  20389, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר באלגוריתמים",  20390, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בביו-אינפורמטיקה",  20552, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר: ממדעי המחשב להוראתם - רקע, התפתחות וסוגיות נבחרות",  20560, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר במערכות מקביליות",  20583, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר מצטיינים במדעי המחשב ובמדעי הקוגניציה",  20921, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר במערכות מידע",  20373, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר באבטחת המרחב המקוון",  20927, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר בבינה חישובית",  20922, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סמינר במדעי המחשב (אנגלית) ",  20954, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה בתכנות מונחה עצמים",  20586, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה בתכנות מתקדם בשפת Java",  20503, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה בבסיסי-נתונים",  20563, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה במערכות הפעלה",  20587, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה בתקשורת מחשבים",  20588, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה במדעי הנתונים",  20936, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה באבטחת מידע",  20931, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+Insert INTO Course (name, number, credits_type, credits_number) VALUES ("סדנה בטכנולוגיות לפיתוח אפליקציות לענן ולרשת",  20995, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
 
 -- Insert data into Dependency table
 INSERT INTO Dependency (dependent_course, base_course) VALUES (20229, "20476, 20109");
 
 -- Insert data into Mandatory_Requirement table
+-- Plan #1
 INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20476);
 INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20109);
 INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20229);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20474);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20475);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20425);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20441);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20407);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20417);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20465);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20440);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20585);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20471);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20466);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20594);
+INSERT INTO mandatory_requirement (plan_id, course_number) VALUES (1, 20905);
 
 -- Insert data into Credits_Requirement table
+-- Plan #1
 INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מתמטיקה", 35);
-INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב", 42);
-INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב", 24);
-INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב", 3);
-INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב", 3);
+INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב", 76);
+INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב מתקדם", 27);
+INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעים", 111);
+INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב מתקדם סמינריוני סדנה", 3);
+INSERT INTO credits_requirement (plan_id, credits_type, credits_number) VALUES (1, "מדעי המחשב מתקדם סמינריוני סמינר", 3);
+
 
 -- Insert data into Course_In_Study_Plan table
+-- Plan #1
 INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20476, 1);
 INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20109, 1);
 INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20229, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20474, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20475, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20425, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20441, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20407, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20417, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20465, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20440, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20585, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20471, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20466, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20594, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20905, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20277, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20436, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20296, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20462, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20551, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20554, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20582, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20937, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20562, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20580, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20364, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20906, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20574, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20581, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20595, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20900, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20942, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20944, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20940, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20946, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20945, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20996, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20997, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20998, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20999, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20368, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20369, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20370, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20371, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20372, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20374, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20375, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20388, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20389, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20390, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20552, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20560, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20583, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20921, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20373, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20927, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20922, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20954, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20586, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20503, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20563, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20587, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20588, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20936, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20931, 1);
+INSERT INTO course_in_study_plan (course_number, plan_id) VALUES (20995, 1);
 
 -- Insert data into Credit_Types table
 INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעי המחשב","מדעי המחשב, מדעי המחשב מתקדם");
-INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעים", "פיזיקה, מתמטיקה, מדעי המחשב, מדעי המחשב מתקדם");
-INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעי המחשב","מדעי המחשב מתקדם");
+INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעים", "פיזיקה, מתמטיקה, מדעי המחשב, מדעי המחשב מתקדם, מדעי המחשב מתקדם סמינריוני סדנה, מדעי המחשב מתקדם סמינריוני סמינר");
+INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעי המחשב מתקדם סמינריוני סמינר", "מדעי המחשב מתקדם סמינריוני סמינר");
+INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעי המחשב מתקדם סמינריוני סדנה", "מדעי המחשב מתקדם סמינריוני סדנה");
+INSERT INTO credit_types (credits_type, sub_type) VALUES ("מדעי המחשב מתקדם","מדעי המחשב מתקדם");
+INSERT INTO credit_types (credits_type, sub_type) VALUES ("מתמטיקה","מתמטיקה");
 
 
