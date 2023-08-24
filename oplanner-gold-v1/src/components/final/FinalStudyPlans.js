1a40595670eaf3b017d7127ca41763d41a6afad3
Yuval Ronen <yuval@interionet.com>
2023-08-24 22:35:12 +0300

    WIP background images

--- oplanner-gold-v1/src/components/final/FinalStudyPlans.js
+++ oplanner-gold-v1/src/components/final/FinalStudyPlans.js
@@ -1,12 +1,18 @@
 import React from "react";
 import CourseTable from '../coursetable/CourseTable'
-
+import backgroundImage from '../../images/finishing.jpeg'
 const FinalStudyPlan = () => {
     const chosenCourses = JSON.parse(localStorage.getItem('chosenCourses'));
     const doNothing = (row) => {}
 
     return (
-        <div>
+        <div style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh', // Ensure the background covers the entire viewport height
+      }}>
             <h1>Final Study Plan</h1>
             <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
                 <div style={{ maxWidth: '800px', width: '100%', padding: '0 20px' }}>
