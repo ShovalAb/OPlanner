b04f72379f2f2757550fdf503954f9479babb32e
MayaGur <mayagur98@gmail.com>
2023-08-26 15:32:06 +0300

    First page

--- oplanner-gold-v1/src/components/studyplans/SubmitButton.js
+++ oplanner-gold-v1/src/components/studyplans/SubmitButton.js
@@ -1,18 +1,30 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import {tabSidebarBGInactiveColor, tabSidebarTextColor} from "../colors";
 
-const SubmitButton = ({selectedStudyPlan}) => {
-    console.log(selectedStudyPlan)
-    if (selectedStudyPlan != undefined && selectedStudyPlan != "-1"){
+const SubmitButton = ({ selectedStudyPlan }) => {
+    console.log(selectedStudyPlan);
+    if (selectedStudyPlan !== undefined && selectedStudyPlan !== "-1") {
+        const buttonStyle = {
+            direction: "rtl", 
+            fontSize: "24px",
+            fontWeight: "bold",
+            width: "200px",
+            height: "50px",
+            backgroundColor: tabSidebarBGInactiveColor, 
+            color: tabSidebarTextColor, 
+            border: "2px solid white",
+        };
+
         return (
             <div>
                 <Link to={"/plan/" + selectedStudyPlan.toString()}>
-                    <button>Start Planning!</button>
+                    <button style={buttonStyle}>התחל לתכנן!</button>
                 </Link>
             </div>
-        )
-        }
+        );
+    }
     return null;
 }
 
-export default SubmitButton
+export default SubmitButton;
